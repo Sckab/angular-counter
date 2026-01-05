b72b7647e450cf916ee529271c1c10df5540a942
Sckab <giulianodeamicis2009@gmail.com>
2026-01-05 16:47:04 +0100

    removed useless signal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core'
+import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { Header } from './components/header/header'
 
@@ -10,6 +10,4 @@ import { Header } from './components/header/header'
   ],
   templateUrl: './app.html',
 })
-export class App {
-  protected readonly title = signal('angular-counter')
-}
+export class App {}
